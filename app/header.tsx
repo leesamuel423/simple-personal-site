dd889bd732b2cc1a142aca1d96274b6b665bfa2c
Samuel Lee <leesamuel423@gmail.com>
2025-04-03 00:45:16 -0400

    Add profile image to header

--- app/header.tsx
+++ app/header.tsx
@@ -5,19 +5,26 @@ import Link from 'next/link'
 export function Header() {
   return (
     <header className="mb-8 flex items-center justify-between">
-      <div>
-        <Link href="/" className="font-medium text-black dark:text-white">
-          Samuel Lee
-        </Link>
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-zinc-600 dark:text-zinc-500"
-          delay={0.5}
-        >
-          Software Engineer
-        </TextEffect>
+      <div className="flex items-center gap-3">
+        <img
+          src="/sam.png"
+          alt="Samuel Lee"
+          className="h-16 w-16 rounded-full"
+        />
+        <div>
+          <Link href="/" className="font-medium text-black dark:text-white">
+            samuel lee
+          </Link>
+          <TextEffect
+            as="p"
+            preset="fade"
+            per="char"
+            className="text-zinc-600 dark:text-zinc-500"
+            delay={0.5}
+          >
+            software engineer
+          </TextEffect>
+        </div>
       </div>
     </header>
   )
